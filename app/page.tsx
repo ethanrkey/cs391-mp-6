ab1bcdb39f0ade83bbfdab6fa036a8aa68e9353d
Ethan Key <ethanrkey23@gmail.com>
2025-04-30 23:11:37 -0400

    styled  a little bit

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,25 @@ export default function Home() {
 
   if (session) {
     return (
-      <div className="p-6">
-        <p>Welcome, {session.user?.name}!</p>
-        <img src={session.user?.image ?? ''} alt="profile pic" width={50} />
-        <p>Email: {session.user?.email}</p>
-        <button onClick={() => signOut()} className="mt-2 underline">Sign out</button>
+      <div className="p-6 flex flex-col">
+        <h1 className="text-4xl pb-10 text-center">Hey!</h1>
+        <div className="flex flex-col gap-3 border-2 rounded-2xl p-8">
+          <p>Welcome, {session.user?.name}!</p>
+          <img src={session.user?.image ?? ''} alt="profile pic" width={50} />
+          <p>Email: {session.user?.email}</p>
+          <button onClick={() => signOut()} className="mt-2 underline">Sign out</button>
+        </div>
       </div>
     );
   }
 
   return (
     <div className="p-6">
-      <p>You are not signed in</p>
-      <button onClick={() => signIn('github')} className="underline">Sign in with GitHub</button>
+      <h1 className="text-4xl pb-10 text-center">CS391 MP6</h1>
+      <div className="flex flex-col gap-3 border-2 rounded-2xl p-8">
+        <p>You are not signed in</p>
+        <button onClick={() => signIn('github')} className="underline">Sign in with GitHub</button>
+      </div>
     </div>
   );
 }
